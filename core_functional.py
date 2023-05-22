4c107e59d4b311216f450a320e44c293881e445f
kindle4jerry <kindle4jerry@gmail.com>
2023-05-22 10:15:27 +0800

    Update core_functional.py

--- core_functional.py
+++ core_functional.py
@@ -73,7 +73,7 @@ def get_core_functions():
                         r"Items need to be transformed:",
             "Suffix":   r"",
             "Visible": False,
-        }
+        },
         "找图片CN": {
             "Prefix":   r"我需要你找一张网络图片。使用Unsplash API(https://source.unsplash.com/960x640/?<英语关键词>)获取图片URL，" +
                         r"然后请使用Markdown格式封装，并且不要有反斜线，不要用代码块。现在，请按以下描述给我发送图片：" + "\n\n",
